ae30debd2ef438667d491704e2d617c396586937
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-06-06 20:57:36 -0500

    Update plugin versions and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.0"
-	kotlin("plugin.spring") version "1.4.0"
+	id("org.springframework.boot") version "2.5.0"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.10"
+	kotlin("plugin.spring") version "1.5.10"
 }
 
 group = "com.eduardguez"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
@@ -22,8 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:8.0.0")
-	runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:8.0.0")
+	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:11.1.0")
+	runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:11.1.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
