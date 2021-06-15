7ac2f69f8fedec00d3d18b0bba33ed4bb6d69dd1
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-06-14 19:42:17 -0500

    Add spring-boot-starter-data-jpa dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.10"
 	kotlin("plugin.spring") version "1.5.10"
+	kotlin("plugin.jpa") version "1.5.10"
 }
 
 group = "com.eduardguez"
@@ -19,6 +20,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
